68154c28b8a35595d60e7bb68e681a4065e76bb8
iker <iker@calypsoai.com>
2022-11-02 19:38:55 +0000

    chore: change location

--- services/api.service.ts
+++ services/api.service.ts
@@ -2,7 +2,7 @@ import { Company, Project } from '../store/models';
 
 const dev = process.env.NODE_ENV !== 'production';
 
-const server = dev ? 'http://localhost:3001' : 'https://boiling-hollows-34647.herokuapp.com';
+const server = dev ? 'http://localhost:3001' : 'https://book-react-next-static-gj56ewo0r-ilopezdeaudikana.vercel.app';
 
 export const getCompanies = async (): Promise<Company[]> => {
   const response: Response = await fetch(`${server}/api/companies`);
