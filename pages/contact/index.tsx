ecde57ad03a674d1f9c64a1361a9b068673bf147
iker <iker@calypsoai.com>
2023-05-19 13:54:21 +0100

    feat: add contact page

---
+++ pages/contact/index.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+const ContactPage = () => {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [status, setStatus] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch('/api/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, message }),
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setStatus('Email sent successfully!');
+    } else {
+      setStatus('Failed to send email. Please try again.');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Send Email</button>
+      </form>
+      {status && <p>{status}</p>}
+    </div>
+  );
+};
+
+export default ContactPage;
