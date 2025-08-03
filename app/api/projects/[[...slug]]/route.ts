bfb102e6aeadc36453fe4b253f70fdcd39e6799f
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2025-08-03 15:01:05 +0100

    use promise

--- app/api/projects/[[...slug]]/route.ts
+++ app/api/projects/[[...slug]]/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 import apiData from '../../../../data'
 import { ProjectDetail, Project } from '../../../types'
 
-export async function GET(request: Request, { params }: { params: { slug?: string | string[] } }) {
-  const { slug  } = params
+export async function GET(request: Request, { params } : { params: Promise<{ slug: string }> }) : Promise<NextResponse> {
+  const { slug  } = await params
   if(!slug) return NextResponse.json<Record<'projects', Project[]>>({ projects: apiData.projects })
   else return NextResponse.json<Record<'project', ProjectDetail | undefined>>({ project: apiData.records.find(record => record.projectId === slug[0])  })
 }
