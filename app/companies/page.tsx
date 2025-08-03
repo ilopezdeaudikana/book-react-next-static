1584da4f78b3a6ebb3452631e6f035c02225a7d2
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2025-08-03 15:47:22 +0100

    debug

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -3,14 +3,14 @@ import { CompanyList } from '../../components/companies/company-list'
 export default async function Companies() {
 
   try {
-    const res = await fetch(`http://localhost:3001/api/companies`)
+    // const res = await fetch(`http://localhost:3001/api/companies`)
 
-    if (!res.ok) {
-      throw ('Failed to fetch data from API')
-    }
-    const { companies } = await res.json()
+    // if (!res.ok) {
+    //   throw ('Failed to fetch data from API')
+    // }
+    const { companies } = { companies: []} // await res.json()
 
-    return <div />
+    return <CompanyList companies={companies} />
   } catch (error) {
     return <p>{error}</p>
   }
