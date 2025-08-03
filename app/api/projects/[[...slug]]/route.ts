ac3c77989a3757b8be34c2c46bc862e6c25d2ca9
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2025-08-03 15:14:12 +0100

    check and reverse

--- app/api/projects/[[...slug]]/route.ts
+++ app/api/projects/[[...slug]]/route.ts
@@ -4,6 +4,6 @@ import { ProjectDetail, Project } from '../../../types'
 
 export async function GET(request: Request, { params } : { params: Promise<{ slug: string }> }) : Promise<NextResponse> {
   const { slug  } = await params
-  if(!slug) return NextResponse.json<Record<'projects', Project[]>>({ projects: apiData.projects })
+  if(!slug) return NextResponse.json<Record<'projects', Project[]>>({ projects: apiData.projects.reverse() })
   else return NextResponse.json<Record<'project', ProjectDetail | undefined>>({ project: apiData.records.find(record => record.projectId === slug[0])  })
 }
--- components/companies/company-list.tsx
+++ components/companies/company-list.tsx
@@ -5,7 +5,7 @@ import {CompanyListRow} from './company-list-row';
 export const CompanyList = ({ companies }) => {
   return (
     <section className="companies">
-      {companies.reverse().map((company: Company) =>
+      {companies && companies.reverse().map((company: Company) =>
         <CompanyListRow key={company.id} company={company} />
       )}
     </section>
