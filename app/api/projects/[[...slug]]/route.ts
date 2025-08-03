062dd505194b9e540544f0196422b2ff41da9aeb
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2025-08-03 14:56:54 +0100

    better type

--- app/api/projects/[[...slug]]/route.ts
+++ app/api/projects/[[...slug]]/route.ts
@@ -2,11 +2,8 @@ import { NextResponse } from 'next/server';
 import apiData from '../../../../data'
 import { ProjectDetail, Project } from '../../../types'
 
-interface RouteContext {
-  params: { slug: string[] }
-}
-export async function GET(request: Request, context: RouteContext) {
-  const { slug  } = context.params
+export async function GET(request: Request, { params }: { params: { slug?: string | string[] } }) {
+  const { slug  } = params
   if(!slug) return NextResponse.json<Record<'projects', Project[]>>({ projects: apiData.projects })
   else return NextResponse.json<Record<'project', ProjectDetail | undefined>>({ project: apiData.records.find(record => record.projectId === slug[0])  })
 }
