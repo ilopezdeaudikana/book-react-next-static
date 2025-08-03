f6036d341aeb0a34c30c245f17591b43469bf78a
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2025-08-03 15:33:57 +0100

    remove faulty import

--- components/companies/company-list.tsx
+++ components/companies/company-list.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch, useAppSelector } from '../../store/hooks'
 import { CompanyListRow } from './company-list-row';
 import { hydrate } from '../../store/reducers/companies.reducer'
 import { Status } from '../../app/types'
+import { useEffect } from 'react'
 export const CompanyList = ({ companies }) => {
   const dispatch = useAppDispatch()
   const { value, status } = useAppSelector((state) => state.companies)
@@ -17,11 +18,10 @@ export const CompanyList = ({ companies }) => {
   }, [dispatch, companies, status])
   return (
     <section className="companies">
-
+      {companies && companies.reverse().map((company: Company) =>
+        <CompanyListRow key={company.id} company={company} />
+      )}
     </section>
   )
 }
 
-function useEffect(arg0: () => void, arg1: any[]) {
-  throw new Error('Function not implemented.')
-}
