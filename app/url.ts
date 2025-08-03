238c7617b36cc7d17183cfac8c16efc3f1c7500d
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2025-08-03 17:03:29 +0100

    fix url

--- app/url.ts
+++ app/url.ts
@@ -1,1 +1,1 @@
-export const url = process.env.NODE_ENV === 'production' ? 'https://book-react-next-static.vercel.app/' : 'http://localhost:3001'
+export const url = process.env.NODE_ENV === 'production' ? 'https://book-react-next-static.vercel.app' : 'http://localhost:3001'
