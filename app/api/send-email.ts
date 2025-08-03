4339b71418b4ba67e59402cd0946ed0b5ab73da9
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2025-08-03 15:04:00 +0100

    type check

--- app/api/send-email.ts
+++ app/api/send-email.ts
@@ -1,26 +1,26 @@
-import sgMail from '@sendgrid/mail';
+import sgMail from '@sendgrid/mail'
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY ?? '')
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { email, message } = req.body;
+    const { email, message } = req.body
 
     const msg = {
       to: process.env.SENDGRID_SENDER,
-      from: process.env.SENDGRID_SENDER,
+      from: process.env.SENDGRID_SENDER ?? 'foo',
       subject: 'Book Contact Form',
       text: `From: ${email}\n\n${message}`,
-    };
+    }
 
     try {
-      await sgMail.send(msg);
-      res.status(200).json({ success: true });
+      await sgMail.send(msg)
+      res.status(200).json({ success: true })
     } catch (error) {
-      console.error('Error sending email:', error);
-      res.status(500).json({ success: false });
+      console.error('Error sending email:', error)
+      res.status(500).json({ success: false })
     }
   } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+    res.status(405).json({ message: 'Method Not Allowed' })
   }
 }
