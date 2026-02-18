8f3785e3c001547128b305f0eb5cc1b539b1340b
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2026-02-18 10:09:19 +0000

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { MenuComponent } from '../components/menu/menu'
 import StoreProvider from './store-provider'
+import { Analytics } from "@vercel/analytics/next"
 import 'antd/dist/reset.css'
 import '../styles/globals.scss'
 
@@ -13,6 +14,7 @@ export default function RootLayout({
       <body>
         <MenuComponent />
         <StoreProvider>{children}</StoreProvider>
+        <Analytics />
       </body>
     </html>
   )
