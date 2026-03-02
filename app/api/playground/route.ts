b0cb55f05e32c2b30d1aea4ce52fc7e14bf61e2f
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2026-03-02 12:40:14 +0000

    debug

--- app/api/playground/route.ts
+++ app/api/playground/route.ts
@@ -3,5 +3,6 @@ import apiData from '../../../data'
 import { Project } from '../../types'
 
 export async function GET() : Promise<NextResponse> {
+  console.log(apiData.playground)
   return NextResponse.json<Record<'projects', Project[]>>({ projects: apiData.playground.reverse() })
 }
