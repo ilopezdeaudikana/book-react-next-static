7fb3cde20e29cacf85ecd5603d5a8f77a9f51377
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2026-03-19 10:16:31 +0000

    put back redirects

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   // Not working with antd modal
   // reactStrictMode: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/projects',
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/projects',
+        permanent: true,
+      },
+    ]
+  }
 }
