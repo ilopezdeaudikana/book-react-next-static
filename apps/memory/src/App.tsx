18ef9a57c8903d1fa26be5468fe19d0d2ce33bb3
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2026-04-01 10:38:02 +0100

    debug memory

--- apps/memory/src/App.tsx
+++ apps/memory/src/App.tsx
@@ -6,26 +6,32 @@ import { Header, InfoPanel, navigateTo } from '@repo/ui'
 import './App.scss'
 
 function App() {
+  const handleNavigation = (path: string) => {
+    console.log('HANDLER', path,)
+    navigateTo(path, true)
+  }
   return (
     <Provider store={store}>
-      <BrowserRouter basename="/memory">
-        <div data-testid='App' className='page'>
-          <Header onNavigate={(path: string) => navigateTo(path, true)} />
+      <div data-testid='App' className='page'>
+        <Header onNavigate={handleNavigation} />
+        <BrowserRouter basename="/memory">
+
           <Routes>
             <Route path='/game' element={<Game />} />
             <Route path='/score' element={<Score />} />
             <Route path='/' element={<Home />} />
           </Routes>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+
+      </div>
       <InfoPanel title="Memory game">
         <p>Stack: React, Typescript, React hooks, Redux, Jest and Scss modules</p>
-        <a 
-          href="https://github.com/ilopezdeaudikana/book-react-next-static/tree/184aaf5351fce2bedbb02c88c64814fe8c67c3b1/apps/memory" 
+        <a
+          href="https://github.com/ilopezdeaudikana/book-react-next-static/tree/184aaf5351fce2bedbb02c88c64814fe8c67c3b1/apps/memory"
           target="_blank"
           rel="noopener noreferrer"
         >
-            See on Github
+          See on Github
         </a>
       </InfoPanel>
     </Provider>
