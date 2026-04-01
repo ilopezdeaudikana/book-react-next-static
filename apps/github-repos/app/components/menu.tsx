543b4a1559f46a7510fe13d2ebee961c8d90ae0c
ilopezdeaudikana <ilopezdeaudikana@gmail.com>
2026-04-01 10:26:17 +0100

    debug n

--- apps/github-repos/app/components/menu.tsx
+++ apps/github-repos/app/components/menu.tsx
@@ -3,9 +3,15 @@
 import { Header, navigateTo } from '@repo/ui'
 
 export const Menu = () => {
+
+  const handleNavigation =  (path: string) => {
+   console.log('HANDLER', path,)
+   navigateTo(path, true)
+  }
+  
   return (
     <>
-      <Header onNavigate={(path: string) => navigateTo(path, true)} />
+      <Header onNavigate={handleNavigation} />
     </>
   )
 }
